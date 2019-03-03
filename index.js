d83782ef7e00db683adf5c0d678f82d215c5aa2b
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-03 22:50:41 +0000

    Express handler for GET, and runnung tests

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 //Set up express app 
 const app = express();
 
+app.get('/api', function(req, res){
+    console.log('GET request');
+    res.end({name: "Lisette"});
+});
+
 //listen for requests 
-app.listen(process.env.port || 4000, function(){
+//   app.listen(process.env.port || 4000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log('now listening for requests');
 });
---
+++ v5NOTES.md
@@ -0,0 +1,13 @@
+VERSION 5 NOTES 
+
+Handleing Requests in Express
+
+now that we have set up our express app and have it listening for requests, we can set up our express handlers 
+
+how is this possible?
+
+when we created the express app we loaded HTTP methods that we can now access and use
+
+- const app = express(); 
+
+
